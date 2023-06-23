9e729bac01b01a1987b38678192bd2108c65524c
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-06-23 20:42:25 +0300

    Create 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+class school {
+  file { '/tmp/school':
+    ensure  => present,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+  }
+}
